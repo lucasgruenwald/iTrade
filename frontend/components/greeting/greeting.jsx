adf8dd539908e7ff45715928525c40543789558c
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-03-05 12:25:48 -0800

    added feather link

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -17,7 +17,10 @@ export const Greeting = ({ currentUser, logout }) => {
             // <div className="greet-no">
                 <header className="home-header">
                     <h1 id="head-1"><Link to="/" id="home-link">iTrade</Link></h1>
-                    <img src="https://images.squarespace-cdn.com/content/53fe4a70e4b0a2293ab0e42a/1409174396100-ZOPXZKUNVM31K4NZHVK0/Robinhood_Glyph_green.png?content-type=image%2Fpng" className="feather-img" />
+                    <a href="https://media0.giphy.com/media/oALtlsKnoRtNS/giphy.webp?cid=790b76113de0ddbbb8880ea860957cab324cf18a7dd5325f&rid=giphy.webp"
+                        target="_blank">
+                        <img src="https://images.squarespace-cdn.com/content/53fe4a70e4b0a2293ab0e42a/1409174396100-ZOPXZKUNVM31K4NZHVK0/Robinhood_Glyph_green.png?content-type=image%2Fpng" 
+                        className="feather-img" /></a>
                     <h1 id="head-2">Products</h1>
                     <h1 id="head-3">Learn</h1>
                     <a href="https://linkedin.com/in/lukegruenwald"><img id="head-4" src="https://img.icons8.com/plasticine/2x/linkedin.png"/></a>
