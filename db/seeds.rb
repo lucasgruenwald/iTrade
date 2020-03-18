63969c51239db0db3898b52a2ee0555878547dab
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-03-17 21:59:22 -0700

    fix deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.destroy_all
-# Holding.destroy_all 
+User.destroy_all
+Holding.destroy_all 
 
 DemoUser = User.create(
     email: "DemoUser@gmail.com", 
