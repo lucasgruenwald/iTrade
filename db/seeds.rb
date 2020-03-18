7b6c29cf18ea4ec2bd8f16b776f686922f2f03d0
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-03-17 23:13:01 -0700

    fix deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Holding.destroy_all 
+# User.destroy_all
+# Holding.destroy_all 
 
 DemoUser = User.create(
     email: "DemoUser@gmail.com", 
@@ -24,37 +24,37 @@ Demo = User.create(
 )
 
 Holding1 = Holding.create(
-    user_id: "16",
+    user_id: 16,
     stock_ticker: "FB",
     share_count: 200
 )
 
 Holding2 = Holding.create(
-    user_id: "16",
+    user_id: 16,
     stock_ticker: "NVTA",
     share_count: 1400
 )
 
 Holding3 = Holding.create(
-    user_id: "16",
+    user_id: 16,
     stock_ticker: "SSYS",
     share_count: 1000
 )
 
 Holding4 = Holding.create(
-    user_id: "16",
+    user_id: 16,
     stock_ticker: "GOOG",
     share_count: 5
 )
 
 Holding5 = Holding.create(
-    user_id: "16",
+    user_id: 16,
     stock_ticker: "TSLA",
     share_count: 7
 )
 
 Holding6 = Holding.create(
-    user_id: "15",
+    user_id: 15,
     stock_ticker: "NCL",
     share_count: 10000
 )
