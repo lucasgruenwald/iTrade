6ddb056d544f9e1dab3fff6e807cd21261200cf7
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-05 16:03:25 -0700

    added social links to footer

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -19,25 +19,51 @@ export const Greeting = ({ currentUser, logout }) => {
 
     const sessionLinks = () => {
         return (
-            // <div className="greet-no">
-                <header className="home-header">
-                    <h1 id="head-1"><Link to="/" id="home-link">iTrade</Link></h1>
-                    <a href="https://i.giphy.com/media/oALtlsKnoRtNS/giphy.webp"
-                        target="_blank">
-                        <img src="https://images.squarespace-cdn.com/content/53fe4a70e4b0a2293ab0e42a/1409174396100-ZOPXZKUNVM31K4NZHVK0/Robinhood_Glyph_green.png?content-type=image%2Fpng" 
-                        className="feather-img" /></a>
-                    {/* <h1 id="head-2">Products</h1>
+          // <div className="greet-no">
+          <header className="home-header">
+            <h1 id="head-1">
+              <Link to="/" id="home-link">
+                iTrade
+              </Link>
+            </h1>
+            <a
+              href="https://i.giphy.com/media/oALtlsKnoRtNS/giphy.webp"
+              target="_blank"
+            >
+              <img
+                src="https://images.squarespace-cdn.com/content/53fe4a70e4b0a2293ab0e42a/1409174396100-ZOPXZKUNVM31K4NZHVK0/Robinhood_Glyph_green.png?content-type=image%2Fpng"
+                className="feather-img"
+              />
+            </a>
+            {/* <h1 id="head-2">Products</h1>
                     <h1 id="head-3">Learn</h1> */}
-                    <a href="https://linkedin.com/in/lukegruenwald" target="_blank">
-                        <img id="head-4" src="https://img.icons8.com/plasticine/2x/linkedin.png"/>
-                    </a>
-                    <div className="header-session">
-                        <h1><Link to="/login" id="sign-in-home">Sign In</Link></h1>
-                        <h1><Link to="/signup" id="signup-home">Sign Up</Link></h1>
-                    </div>
-                </header>
-            // </div>
-        )
+            <a href="https://github.com/lucasgruenwald" target="_blank">
+              <img
+                id="head-3"
+                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+              />
+            </a>
+            <a href="https://linkedin.com/in/lukegruenwald" target="_blank">
+              <img
+                id="head-4"
+                src="https://img.icons8.com/plasticine/2x/linkedin.png"
+              />
+            </a>
+            <div className="header-session">
+              <h1>
+                <Link to="/login" id="sign-in-home">
+                  Sign In
+                </Link>
+              </h1>
+              <h1>
+                <Link to="/signup" id="signup-home">
+                  Sign Up
+                </Link>
+              </h1>
+            </div>
+          </header>
+          // </div>
+        );
     }
 
     return currentUser  ? loggedGreet() : sessionLinks();
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -7,100 +7,117 @@ function Home (){
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
     return (
-  <div className="home">
-    <div className="home-img">
-      <div className="home-left-text">
-        <h1>It’s Time to Do Money</h1>
-        <h3>
-          iTrade, a pioneer of commission-free investing, gives you more ways to
-          make your money work harder.
-        </h3>
-        <button className="body-sign-up">
-          <Link to="/signup" id="body-sign-up-link">
-            Sign Up
-          </Link>
-        </button>
-      </div>
-      <img
-        className="home-first-img"
-        src="https://cdn.robinhood.com/assets/robinhood/brand_2/2d98ce66a6fc8e2a3e403064eefb81a1-2x.png"
-      />
-    </div>
-    <div className="break-free">
-      <h2>Break Free from Commission Fees</h2>
-      <p>Make unlimited commission-free stock trades with iTrade Financial.</p>
-
-      <button onClick={() => setModalIsOpen(true)} className="disclosure-button">
-        Commissions Disclosure
-      </button>
-
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        className="modal-disclosure"
-        id="modal-disclosure"
-        ariaHideApp = { false }
-        style={
-            {
-                overlay: {
-                    backgroundColor: 'rgba(1,0,0,0.8)'
-                }
-            }
-        }
-      >
-
-        <button onClick={() => setModalIsOpen(false)} className="close-modal">&times;</button>
-        <div className="modal-title">Commissions Disclosure</div>
-
-        <div className="modal-body">
+      <div className="home">
+        <div className="home-img">
+          <div className="home-left-text">
+            <h1>It’s Time to Do Money</h1>
+            <h3>
+              iTrade, a pioneer of commission-free investing, gives you more
+              ways to make your money work harder.
+            </h3>
+            <button className="body-sign-up">
+              <Link to="/signup" id="body-sign-up-link">
+                Sign Up
+              </Link>
+            </button>
+          </div>
+          <img
+            className="home-first-img"
+            src="https://cdn.robinhood.com/assets/robinhood/brand_2/2d98ce66a6fc8e2a3e403064eefb81a1-2x.png"
+          />
+        </div>
+        <div className="break-free">
+          <h2>Break Free from Commission Fees</h2>
           <p>
-            Commission-free trading means $0 commission trades placed on
-            self-directed accounts via mobile devices or web. Keep in mind,
-            other fees may still apply. Please see the Robinhood Financial Fee
-            Schedule to learn more.
+            Make unlimited commission-free stock trades with iTrade Financial.
           </p>
+
+          <button
+            onClick={() => setModalIsOpen(true)}
+            className="disclosure-button"
+          >
+            Commissions Disclosure
+          </button>
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setModalIsOpen(false)}
+            className="modal-disclosure"
+            id="modal-disclosure"
+            ariaHideApp={false}
+            style={{
+              overlay: {
+                backgroundColor: "rgba(1,0,0,0.8)",
+              },
+            }}
+          >
+            <button
+              onClick={() => setModalIsOpen(false)}
+              className="close-modal"
+            >
+              &times;
+            </button>
+            <div className="modal-title">Commissions Disclosure</div>
+
+            <div className="modal-body">
+              <p>
+                Commission-free trading means $0 commission trades placed on
+                self-directed accounts via mobile devices or web. Keep in mind,
+                other fees may still apply. Please see the Robinhood Financial
+                Fee Schedule to learn more.
+              </p>
+            </div>
+          </Modal>
         </div>
 
-      </Modal>
-    </div>
-        
-    <div className="marquee"></div>
-    <div className="global-section">
-      <h1 className="global-title">
-        <p>Invest in global companies</p>
-        <p>in a whole new way.</p>
-      </h1>
+        <div className="marquee"></div>
+        <div className="global-section">
+          <h1 className="global-title">
+            <p>Invest in global companies</p>
+            <p>in a whole new way.</p>
+          </h1>
 
-      <div className="global-companies">
-        <div className="global-1">
-          <p></p>
+          <div className="global-companies">
+            <div className="global-1">
+              <p></p>
+            </div>
+            <div className="global-2">
+              <p></p>
+            </div>
+            <div className="global-3">
+              <p></p>
+            </div>
+          </div>
         </div>
-        <div className="global-2">
-          <p></p>
-        </div>
-        <div className="global-3">
-          <p></p>
-        </div>
-      </div>
-    </div>
-    <div>
-      <img
-        src="https://www.stupidgag.com/wp-content/uploads/2019/09/wolf-of-wall-street-movie-poster.jpg"
-        className="home-temp-img"
-      />
-    </div>
-
-    <footer>
         <div>
+          <img
+            src="https://www.stupidgag.com/wp-content/uploads/2019/09/wolf-of-wall-street-movie-poster.jpg"
+            className="home-temp-img"
+          />
+        </div>
+
+        <footer>
+          <div>
             <p>Website by Luke Gruenwald</p>
             <p>lgruenwald@ucdavis.edu</p>
             <p>(415) 260-4991</p>
-        </div>
-        <div>
-
-        </div>
-    </footer>
-  </div>
-)};
+          </div>
+          <div>
+            <a href="https://linkedin.com/in/lukegruenwald" target="_blank">
+              <img
+                id="foot-1"
+                src="https://img.icons8.com/plasticine/2x/linkedin.png"
+              />
+            </a>
+            <a href="https://github.com/lucasgruenwald" target="_blank">
+              <img
+                id="foot-2"
+                src="https://toppng.com/uploads/preview/see-all-open-source-repositories-github-logo-11563031735gubsot66ry.png"
+              />
+            </a>
+          </div>
+        </footer>
+      </div>
+    );};
 
 export default Home;
