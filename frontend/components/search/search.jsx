81eec4810d3f60f2e520d95f809a9463aa181d33
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-08 00:54:26 -0700

    remove redundancy

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -2,15 +2,47 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            search: '',
+            results: []
+        }
+
+        this.handleClearBar = this.handleClearBar.bind(this)
+        this.update = this.update.bind(this)
+    }
+
+    handleClearBar() {
+        this.setState({ search: '' });
+    }
+
+    update(entry) {
+        return e => {
+            this.setState({
+                [entry]: e.currentTarget.value,
+            })
+        }
+    }
 
     render(){
+
+        let suggestions = []
+
         return(
 
-            <input className="search-bar"
-                type="text"
-                placeholder="change url  e.g.  #/stock/AAPL"    
-            >
-            </input>
+            <div className="search-bar">
+                <input 
+                    className='search-input' 
+                    type="text" 
+                    placeholder="url e.g. #/stock/AAPL"
+                    value={this.state.search} 
+                    onChange={this.update("search")} 
+                />
+                <div className="dropdown">
+                    {suggestions}
+                </div>
+            </div>
 
         )
     }
