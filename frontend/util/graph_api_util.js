16696a4bebe2d82363eab19d81f1c9795cf5d461
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-19 18:35:12 -0700

    encrypt api credentials

--- frontend/util/graph_api_util.js
+++ frontend/util/graph_api_util.js
@@ -1,13 +1,13 @@
 export const fetchDailyPrices = symbol => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/stable/stock/${symbol}/intraday-prices?token=pk_c12f0279a58e44af91d34d80a582cabd`
+        url: `https://cloud.iexapis.com/stable/stock/${symbol}/intraday-prices?token=${window.iexAPIKey}`
     })
 )
 
 export const fetchPrices = (symbol, timeFrame) => {
     return $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/stable/stock/${symbol}/chart/${timeFrame}?chartIEXOnly=true&token=pk_c12f0279a58e44af91d34d80a582cabd`,
+        url: `https://cloud.iexapis.com/stable/stock/${symbol}/chart/${timeFrame}?chartIEXOnly=true&token=${window.iexAPIKey}`,
     })
 }
--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,13 +1,13 @@
 export const fetchNews = () => {
     return $.ajax({
         method: "GET",
-        url: `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=30c43750465046fd8142ea8a03548126`
+        url: `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${window.newsAPIKey}`
     })
 }
 
 export const fetchOneNews = (ticker) => {
     return $.ajax({
         method: "GET",
-        url: `https://newsapi.org/v2/everything?q=${ticker}&apiKey=30c43750465046fd8142ea8a03548126` 
+        url: `https://newsapi.org/v2/everything?q=${ticker}&apiKey=${window.newsAPIKey}` 
     })
 }
