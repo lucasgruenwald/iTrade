0b7ee6bc948d3dc95f238aa7125f255b893b7bac
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-28 16:47:00 -0700

    prep for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Holding.destroy_all 
+User.destroy_all!
+Holding.destroy_all!
 
 Demo = User.create!(
     email: "iTradeDemoUser@gmail.com", 
