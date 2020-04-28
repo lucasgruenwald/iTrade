6d13ccb2fb3884ce1c9ac32c133bff057acdfbc6
Luke Gruenwald <lgruenwald@sbcglobal.net>
2020-04-28 15:28:25 -0700

    display estimated value of transaction

--- frontend/components/stock/transaction.jsx
+++ frontend/components/stock/transaction.jsx
@@ -29,6 +29,12 @@ class TransactionForm extends React.Component {
         this.changeUnderline(dir);
         // this.props.clearErrors();
     }
+    
+    updateShares() {
+        return e => {
+            this.setState({ purchase_shares: parseInt(e.target.value) })
+        }
+    }
 
     changeUnderline(dir) {
         let dirs = Array.prototype.slice.call(document.querySelectorAll('.buy, .sell'));
@@ -43,7 +49,12 @@ class TransactionForm extends React.Component {
 
     render(){
 
-
+        console.log(this.props.price)
+        console.log(this.state.purchase_shares)
+        let estCost = this.state.purchase_shares ? 
+            (this.state.purchase_shares * Number((this.props.price).replace(/[^0-9\.-]+/g, ""))).toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+            : "$0"
+    
 
         return(
 
@@ -54,7 +65,14 @@ class TransactionForm extends React.Component {
                 </div>
                 <div className="flex-transaction">
                     <p className="shares-text">Shares</p>
-                    <input type="number" placeholder="" className="shares-input"></input>
+                    <input  
+                        type="number" 
+                        placeholder="0" 
+                        id="shares-input" 
+                        className="shares-input"
+                        value={this.state.purchase_shares}
+                        onChange={this.updateShares()} 
+                    />
                 </div>
                 <div className="flex-transaction">
                     <p className="market-text">Market Price</p>
@@ -62,7 +80,7 @@ class TransactionForm extends React.Component {
                 </div>
                 <div className="flex-transaction">
                     <p className="cost-text">Estimated Value</p>
-                    
+                    <p className="cost-value">{estCost}</p>
                 </div>           
                 <button className="place-order">Place Order</button>
                 <p className="buy-avail-cash">{this.state.cash.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}&nbsp; Buying Power Available</p>
