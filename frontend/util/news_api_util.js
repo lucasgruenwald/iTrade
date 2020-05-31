d6335d012058876eebaca65f4ef7ed219f97b68b
lukegruenwald <lgruenwald@sbcglobal.net>
2020-05-31 01:31:02 -0700

    remove cors url from news request

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,13 +1,13 @@
 export const fetchNews = () => {
     return $.ajax({
         method: "GET",
-        url: `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${window.newsAPIKey}`
+        url: `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${window.newsAPIKey}`
     })
 }
 
 export const fetchOneNews = (ticker) => {
     return $.ajax({
         method: "GET",
-        url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${ticker}&apiKey=${window.newsAPIKey}` 
+        url: `https://newsapi.org/v2/everything?q=${ticker}&apiKey=${window.newsAPIKey}` 
     })
 }
