835c57d9ad5593f4a50838616f51e78dabe92ac2
lukegruenwald <lgruenwald@sbcglobal.net>
2020-06-02 22:29:31 -0700

    re-add cors url

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,13 +1,13 @@
 export const fetchNews = () => {
     return $.ajax({
         method: "GET",
-        url: `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${window.newsAPIKey}`
+        url: `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${window.newsAPIKey}`
     })
 }
 
 export const fetchOneNews = (ticker) => {
     return $.ajax({
         method: "GET",
-        url: `https://newsapi.org/v2/everything?q=${ticker}&apiKey=${window.newsAPIKey}` 
+        url: `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything?q=${ticker}&apiKey=${window.newsAPIKey}` 
     })
 }
